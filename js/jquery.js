0f6f5535c4c8126701f0e253227811d2420eed3d
ryun56 <wakwakugit@gmail.com>
2020-02-23 18:36:07 +0900

    ページトップへスクロールするボタン追加

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,20 @@
 $(function(){
-    //スムーズなクスクロール
-
     // トップへスクロール
-
-
-
-
-
-
+    var pagetop = $('.pagetop');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
     //ページ最終更新日の表示
 
+    
     //フォーム選択時にラベルの色を変更→フォーム入力時に、今どの項目を選択しているのかがわかりやすくなる
     $('#name, #email,#message')
     .focusin(function(e) {
